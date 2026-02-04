8ef527d432dd8599ef73e7a5ad9f422123f0e4fc
jardelMessias39 <jardelmessias28@gmail.com>
2026-02-03 22:57:30 -0300

    vamos ver se vai

--- meu-portfolio-jardel/frontend/src/components/AvatarFalante.jsx
+++ meu-portfolio-jardel/frontend/src/components/AvatarFalante.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Mic } from 'lucide-react';
 
 const AvatarFalante = ({
-imagemParado = "/avatar-parado.png",
+imagemParado = "/avatar-parado2.jpeg",
   videoFalando = "/avatar-falando.mp4",
   tamanho = 300
 }) => {
